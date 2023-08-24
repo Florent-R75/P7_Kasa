a7269829f961409e9ca0b9526c85b9913b4d978f
FlorentRATANAVANH <florent@me.com>
2023-08-24 16:18:20 +0200

    1er commit : Router

---
+++ src/components/PageNav.jsx
@@ -0,0 +1,18 @@
+import { NavLink } from "react-router-dom";
+
+function PageNav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/">Accueil</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">A Propos</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default PageNav;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Homepage from "./pages/Homepage";
+import Room from "./pages/Room";
+import About from "./pages/About";
+import PageNotFound from "./pages/PageNotFound";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="room" element={<Room />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <App />
+    </Router>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
